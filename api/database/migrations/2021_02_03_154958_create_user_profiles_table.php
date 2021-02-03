00508bc16d5c79199b8c38855060af780e843154
JasonChan <jasonnchann24@gmail.com>
2021-02-03 15:57:38 +0700

    create Userprofile model migration

---
+++ api/database/migrations/2021_02_03_154958_create_user_profiles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('country');
+            $table->text('about');
+            $table->string('facebook_link', 512);
+            $table->string('linkedin_link', 512);
+            $table->string('github_link', 512);
+            $table->string('others_link', 512);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
