4b9a36ea397c73a7069702d544e968b1c75679e4
JasonChan <jasonnchann24@gmail.com>
2021-02-06 22:33:32 +0700

    user user profile seeder random suspend

--- api/database/seeders/User/UserProfileSeeder.php
+++ api/database/seeders/User/UserProfileSeeder.php
@@ -21,11 +21,17 @@ class UserProfileSeeder extends Seeder
 
         foreach ($users as $user) {
             $createProfile = (bool)random_int(0, 1);
+            $suspended = random_int(0, 5);
             if ($createProfile) {
                 $user->profile()->save(
                     UserProfile::factory()
                         ->makeOne()
                 );
+            }
+
+            if ($suspended == 1) {
+                $user->is_suspended = true;
+                $user->save();
             }
         }
     }
