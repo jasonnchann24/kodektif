4ab980f1225dc7785f1a412430b35ceda13e7403
JasonChan <jasonnchann24@gmail.com>
2021-02-06 15:03:40 +0700

    initial only admin middleware

---
+++ api/tests/Feature/Http/Controllers/SuspendControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SuspendControllerTest extends TestCase
+{
+}
