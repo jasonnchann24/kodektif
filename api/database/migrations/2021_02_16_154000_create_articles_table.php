89293ba6a4c0c0bbac602c2c072f63165254b1e8
JasonChan <jasonnchann24@gmail.com>
2021-02-16 17:06:15 +0700

    scaffold Article

---
+++ api/database/migrations/2021_02_16_154000_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('language_id')->constrained();
+            $table->string('title', 512);
+            $table->text('description');
+            $table->longText('body');
+            $table->string('slug', 1500);
+            $table->bigInteger('likes_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
