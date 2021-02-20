f350ad102c27c8e51a0a09352a8e4f3f16a8ccd3
JasonChan <jasonnchann24@gmail.com>
2021-02-20 20:56:44 +0700

    add policy for post

--- api/app/Http/Controllers/Post/PostController.php
+++ api/app/Http/Controllers/Post/PostController.php
@@ -8,22 +8,17 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['auth:sanctum', 'not.suspended'])->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
         //
     }
@@ -46,17 +41,6 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
     {
         //
     }
--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Article;
+use App\Models\Post;
 use App\Models\UserProfile;
 use App\Policies\ArticlePolicy;
+use App\Policies\PostPolicy;
 use App\Policies\UserProfilePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -18,7 +20,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         UserProfile::class => UserProfilePolicy::class,
-        Article::class => ArticlePolicy::class
+        Article::class => ArticlePolicy::class,
+        Post::class => PostPolicy::class
     ];
 
     /**
