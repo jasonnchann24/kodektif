9a44d23be37757ad71f42e5d428923abf351b1d7
JasonChan <jasonnchann24@gmail.com>
2021-02-21 19:05:03 +0700

    initial scaffold post comment controller test model migration

---
+++ api/app/Http/Controllers/Post/PostComment/PostCommentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Post\PostComment;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post\PostComment\PostComment;
+use Illuminate\Http\Request;
+
+class PostCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post\PostComment\PostComment  $postComment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostComment $postComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post\PostComment\PostComment  $postComment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PostComment $postComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post\PostComment\PostComment  $postComment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostComment $postComment)
+    {
+        //
+    }
+}
---
+++ api/app/Models/Post/PostComment/PostComment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Post\PostComment;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostComment extends Model
+{
+    use HasFactory;
+}
