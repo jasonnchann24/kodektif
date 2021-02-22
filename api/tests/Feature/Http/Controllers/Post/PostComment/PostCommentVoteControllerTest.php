0249510621c6f3e1805162807a5518763c8f326e
JasonChan <jasonnchann24@gmail.com>
2021-02-22 21:59:25 +0700

    postcommentvote controller test initial

---
+++ api/tests/Feature/Http/Controllers/Post/PostComment/PostCommentVoteControllerTest.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Post\PostComment;
+
+use App\Models\Post\PostComment\PostComment;
+use App\Models\Post\PostComment\PostCommentVote;
+use App\Models\User;
+use Database\Seeders\Post\PostComment\PostCommentSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tests\TestTraits\CreateUserTrait;
+
+class PostCommentVoteControllerTest extends TestCase
+{
+    use RefreshDatabase, CreateUserTrait;
+
+    protected $postComments;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed([
+            UserProfileSeeder::class,
+            RoleSeeder::class,
+            CategorySeeder::class,
+            LanguageSeeder::class,
+            PostSeeder::class,
+            PostCommentSeeder::class
+        ]);
+
+        $this->postComments = PostComment::all();
+    }
+
+    /** @test */
+    public function non_authenticated_user_can_not_access_these_comment_vote_apis()
+    {
+        $this->postJson(route('post-comment-votes.store'), [])
+            ->assertStatus(401);
+
+        $this->patchJson(route('post-comment-votes.update', ['post_comment_vote' => -1]), [])
+            ->assertStatus(401);
+
+
+        $this->deleteJson(route('post-comment-votes.destroy', ['post_comment_vote' => 1]))
+            ->assertStatus(401);
+    }
+
+    /** @test */
+    public function suspended_user_can_not_access_these_comment_vote_apis()
+    {
+        $user = $this->createBasicUser();
+        $user = $this->suspendUser($user);
+
+        $this->actingAs($user);
+        $this->postJson(route('post-comment-votes.store'), [])
+            ->assertStatus(403);
+
+        $this->patchJson(route('post-comment-votes.update', ['post_comment_vote' => -1]), [])
+            ->assertStatus(403);
+
+        $this->deleteJson(route('post-comment-votes.destroy', ['post_comment_vote' => -1]))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    public function user_can_upvote_comment()
+    {
+        $user = $this->createBasicUser();
+
+        $postComment = $this->getOnePostComment();
+
+        $this->createPostCommentVote($user, $postComment, true);
+    }
+
+    /** @test */
+    public function user_can_downvote_comment()
+    {
+        $user = $this->createBasicUser();
+
+        $postComment = $this->getOnePostComment();
+
+        $this->createPostCommentVote($user, $postComment, false);
+    }
+
+    /** @test */
+    public function user_can_not_double_vote_comment()
+    {
+        $user = $this->createBasicUser();
+        $postComment = $this->getOnePostComment();
+        $this->createPostCommentVote($user, $postComment, true);
+        $this->createPostCommentVote($user, $postComment, true);
+
+        $commentVote = PostCommentVote::where('user_id', $user->id)
+            ->where('post_comment_id', $postComment)
+            ->count();
+        $this->assertEquals(1, $commentVote, 'user can have multiple vote in the same comment');
+    }
+
+    /** @test */
+    public function user_can_update_own_vote()
+    {
+        $user = $this->createBasicUser();
+        $postComment = $this->getOnePostComment();
+
+        $vote = $this->createPostCommentVote($user, $postComment, true);
+
+        $this->actingAs($user)
+            ->patchJson(route('post-comment-votes.update', ['post_comment_vote' => $vote->id]), ['upvote' => false])
+            ->assertStatus(200);
+
+        $updated = PostCommentVote::find($vote->id);
+        $this->assertEquals(false, (bool)$updated->upvote, 'not updated');
+    }
+
+    /** @test */
+    public function user_can_not_update_others_vote()
+    {
+        $user = $this->createBasicUser();
+        $userTwo = $this->createBasicUser();
+
+        $postComment = $this->getOnePostComment();
+        $vote = $this->createPostCommentVote($user, $postComment, false);
+
+        $this->actingAs($userTwo)
+            ->patchJson(route('post-comment-votes.update', ['post_comment_vote' => $vote->id]), ['upvote' => true])
+            ->assertStatus(403);
+
+        $target = PostCommentVote::find($vote->id);
+        $this->assertEquals(false, (bool)$target->upvote, 'other user can update vote.');
+    }
+
+    /** @test */
+    public function user_can_delete_own_vote()
+    {
+        $user = $this->createBasicUser();
+        $postComment = $this->getOnePostComment();
+        $vote = $this->createPostCommentVote($user, $postComment, false);
+
+        $this->actingAs($user)
+            ->deleteJson(route('post-comment-votes.destroy', ['post_comment_vote' => $vote->id]))
+            ->assertStatus(204);
+
+        $target = PostCommentVote::where('user_id', $user->id)
+            ->where('post_comment_id', $postComment->id)
+            ->count();
+
+        $this->assertEquals(0, $target, 'vote not deleted.');
+    }
+
+    /** @test */
+    public function user_can_not_delete_others_vote()
+    {
+        $user = $this->createBasicUser();
+        $userTwo = $this->createBasicUser();
+        $postComment = $this->getOnePostComment();
+        $vote = $this->createPostCommentVote($user, $postComment, false);
+
+        $this->actingAs($userTwo)
+            ->deleteJson(route('post-comment-votes.destroy', ['post_comment_vote' => $vote->id]))
+            ->assertStatus(403);
+
+        $target = PostCommentVote::where('user_id', $user->id)
+            ->where('post_comment_id', $postComment->id)
+            ->count();
+
+        $this->assertEquals(1, $target, 'vote deleted by other user.');
+    }
+
+    protected function getOnePostComment(): PostComment
+    {
+        return $this->comments->random(1)->first();
+    }
+
+    protected function createPostCommentVote(User $user, PostComment $postComment, bool $upvote): PostCommentVote
+    {
+        $this->actingAs($user);
+
+        $payload = [
+            'post_comment_id' => $postComment->id,
+            'upvote' => $upvote
+        ];
+
+        $response = $this->postJson(('post-comment-votes.store'), $payload)
+            ->assertStatus(201);
+
+        $payload['user_id'] = $user->id;
+        $this->assertDatabaseHas('post_comment_votes', $payload);
+
+        return PostCommentVote::findOrFail($response['id']);
+    }
+}
