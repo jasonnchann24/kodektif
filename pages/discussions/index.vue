13f19053583405f757dd8c9f12322a3d7b9a1abc
JasonChan <jasonnchann24@gmail.com>
2021-04-19 11:01:05 +0700

    discussion initial

--- pages/discussions/index.vue
+++ pages/discussions/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    discussions
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Discussions</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
