3be77a8b133a0f89a0d8c18052b67a6a6c00ab31
Jason Chan <jasonnchann24@gmail.com>
2021-06-24 13:21:44 +0700

    favicon new

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Kodektif',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
